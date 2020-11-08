e86ed33925a503b3c24ae847bcdee2a2a7b869ca
Romail asad <romailasad2@gmail.com>
2020-11-09 00:00:11 +0500

    updated menu-item with withRouter and added shop/hats route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
 import { Homepage } from './Components/Pages/Homepage/Homepage.component';
+
+const Hats = () => (
+    <div>
+        <h1>Hats Page</h1>
+    </div>
+)
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
-            <Homepage />
+                <Switch>
+                <Route exact path='/' component={Homepage}/>
+                <Route path='/shop/hats' component={Hats}/>
+                </Switch>
             </div>
         )
     }
